cc8a5b4175b09b8b878a8e1ea1875e34c5dbf0b2
Paul McCarthy <paulmccarthy2010@live.ca>
2016-07-04 15:16:21 +0100

    Fizz buzz

---
+++ Fizzbuzz.rb
@@ -0,0 +1,19 @@
+def fizzBuzz(num)
+
+	if num % 3 == 0 and num % 5 ==0
+		 "FizzBuzz"
+	elsif num%5 == 0
+		 "Buzz"
+	elsif num%3 == 0
+		"fizz"
+	else
+		num
+	end
+end
+
+min = 1
+max = 100
+
+min.upto(max) do |num|
+	puts fizzBuzz(num)
+end
